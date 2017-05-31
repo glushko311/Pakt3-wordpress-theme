4dea6adaff24e71c53097dd26de8215c17fb6c69
Vadim Glushko <glushko311@gmail.com>
2017-05-31 12:08:04 +0300

    доп.стили для правострорнних языков

--- footer.php
+++ footer.php
@@ -1,124 +1,98 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package pakt3
- */
-
-// langArr[[russian, english, ukrainian, idish], ...] куда добавить значение определяется по номеру
-
-$langArr = [["о нас","about us","о нас","о нас"],
-		    ["Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях.",
-		     "Lorem ipsum dolor sit amet, con sectetuer adipiscing elit, sed diam non ummy nibh euismod tincidunt ut lao reet dolore magna aliquam. erat volutpat.",
-		     "Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил, что он у себя в постели превратился в страшное насекомое. ",
-		     "Lorem ipsum dolor sit amet, con sectetuer adipiscing elit, sed diam non ummy nibh euismod tincidunt ut lao reet dolore magna aliquam. erat volutpat."
-			],
-			["Adam Smith","Adam Smith","о нас","о нас"],
-			["Director","Director","о нас","о нас"],
-			["ссылки","links","о нас","о нас"],
-			["Crowd Management","Crowd Management","о нас","о нас"],
-			["Corporate Governance","Corporate Governance","о нас","о нас"],
-			["VIP Luxury Living","VIP Luxury Living","о нас","о нас"],
-			["Vehicle Escort","Vehicle Escort","о нас","о нас"],
-			["Gated Community Services","Gated Community Services","о нас","о нас"],
-			["Chauffeur Services","Chauffeur Services","о нас","о нас"],
-			["Special Events","Special Events","о нас","о нас"],
-			["Primary contacts","Primary contacts","о нас","о нас"],
-			["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, nihil.","Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, nihil.","о нас","о нас"],
-			["Sharp Company<br>8901 Marmora Road, Glasgow, D04 89GR","Sharp Company<br>8901 Marmora Road, Glasgow, D04 89GR","о нас","о нас"],
-			["Tel: 1-800-234-5678","Tel: 1-800-234-5678","о нас","о нас"],
-			["copyright","copyright","о нас","о нас"],
-			["Sharp","Sharp","о нас","о нас"],
-			["Privacy Policy","Privacy Policy","о нас","о нас"],
-];
-
-switch(get_marker_of_page()){
-	case "ru":
-		$lang_marker = 0;
-		break;
-
-	case "en":
-		$lang_marker = 1;
-		break;
-
-	case "ua":
-		$lang_marker = 2;
-		break;
-
-	case "ih":
-		$lang_marker = 3;
-		break;
-
-	default:
-		$lang_marker = 0;
-}
-
-
-?>
-	</div><!-- #content -->
-		<footer>
-			<div class="container">
-				<div class="row">
-					<article class="span3">
-						<h3><span><?= $langArr[0][$lang_marker];?></span></h3>
-						<p class="title1">&ldquo;<?= $langArr[1][$lang_marker];?>  &rdquo;</p>
-						<p class="title2"><?= $langArr[2][$lang_marker];?> </p>
-						<p class="title3"><?= $langArr[3][$lang_marker];?> </p>
-					</article>
-					<article class="span3">
-						<h3><span><?= $langArr[4][$lang_marker];?></span></h3>
-						<ul class="list1">
-							<li><a href="#"><?= $langArr[5][$lang_marker];?><em></em></a></li>
-							<li><a href="#"><?= $langArr[6][$lang_marker];?><em></em></a></li>
-							<li><a href="#"><?= $langArr[7][$lang_marker];?><em></em></a></li>
-							<li><a href="#"><?= $langArr[8][$lang_marker];?><em></em></a></li>
-							<li><a href="#"><?= $langArr[9][$lang_marker];?><em></em></a></li>
-							<li><a href="#"><?= $langArr[10][$lang_marker];?><em></em></a></li>
-							<li><a href="#"><?= $langArr[11][$lang_marker];?><em></em></a></li>
-						</ul>
-					</article>
-					<article class="span3">
-						<h3><span><?= $langArr[12][$lang_marker];?></span></h3>
-						<p class="margBot1"><?= $langArr[13][$lang_marker];?>﻿</p>
-						<p class="margBot1"><span><?= $langArr[14][$lang_marker];?></span></p>
-						<p><span><?= $langArr[15][$lang_marker];?></span></p>
-					</article>
-					<article class="span3">
-						<h3><span><?= $langArr[16][$lang_marker];?></span></h3>
-						<p><?= $langArr[17][$lang_marker];?> &copy; 2013 &bull; <br><a href="index-5.html"><?= $langArr[18][$lang_marker];?></a></p>
-						<ul class="follow_icon">
-							<li><a href="#"><img alt="" src="<?php bloginfo("template_directory"); ?>/img/follow_icon1.png"></a></li>
-							<li><a href="#"><img alt="" src="<?php bloginfo("template_directory"); ?>/img/follow_icon2.png"></a></li>
-							<li><a href="#"><img alt="" src="<?php bloginfo("template_directory"); ?>/img/follow_icon3.png"></a></li>
-						</ul>
-
-					</article>
-
-				</div>
-<!--				More Security Website Templates at <a rel="nofollow" href="http://www.templatemonster.com/category/security-web-templates/" target="_blank">TemplateMonster.com</a>-->
-			</div>
-		</footer>
-
-		<script>
-			$('#search a').hover(function(){
-				$(this).stop().animate({'opacity':0.5},350, "easeOutSine");
-			}, function(){
-				$(this).stop().animate({'opacity':1},350, "easeOutSine");
-			})
-			$('.follow_icon a').hover(function(){
-				$(this).stop().animate({'opacity':0.5},350, "easeOutSine");
-			}, function(){
-				$(this).stop().animate({'opacity':1},350, "easeOutSine");
-			})
-		</script>
-
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package pakt3
+ */
+
+// langArr[[russian, english, ukrainian, idish], ...] куда добавить значение определяется по номеру
+
+$langArr = [["КОНТАКТЫ","CONTACTS","КОНТАКТИ","КОНТАКТЫ"],
+
+			["тел.:<span> +38 (067) 669 50 61 </span>","phone:<span> +38 (067) 669 50 61 </span>","тел.:<span> +38 (067) 669 50 61 </span>","тел.:<span> +38 (067) 669 50 61 </span>"],
+			["тел.:<span>+38 (050) 850 61 23 </span>","phone:<span>+38 (050) 850 61 23 </span>","тел.:<span>+38 (050) 850 61 23 </span>","тел.:<span>+38 (050) 850 61 23 </span>"],
+			["e-mail:<span> info@pakt3.com.ua </span>","e-mail:<span> info@pakt3.com.ua </span>","e-mail:<span> info@pakt3.com.ua </span>","e-mail:<span> info@pakt3.com.ua </span>"],
+			["О нас","About us","Про нас","О нас"],
+			["Группа компаний Пакт-3 - команда профессионалов в области комплексной безопасности, аудита, предоставлении юридических услуг, внешнеэкономической деятельности, рекрутинга, кинологии и обеспечения комплекса мероприятий для комплексного обеспечения потребностей клиента.",
+					"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo 	ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+					"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo 	ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+					"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo 	ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem."],
+		    ["Группа компаний \"Пакт-3\" © 2017","Group of companies \"Pact-3\" © 2017","Група компаній \"Пакт-3\" © 2017","Группа компаний \"Пакт-3\" © 2017"],
+		    ["Разработано компанией","Developed by the company","Розроблено компанією","Разработано компанией"],
+];
+
+switch(get_marker_of_page()){
+	case "ru":
+		$lang_marker = 0;
+		break;
+
+	case "en":
+		$lang_marker = 1;
+		break;
+
+	case "ua":
+		$lang_marker = 2;
+		break;
+
+	case "ih":
+		$lang_marker = 3;
+		break;
+
+	default:
+		$lang_marker = 0;
+}
+
+
+?>
+	</div><!-- #content -->
+<footer>
+	<div class="container">
+		<div class="row">
+			<article class="span6">
+				<h3><span><?php echo $langArr[0][$lang_marker]; ?></span></h3>
+				<a class="title1" href="tel:+38 (067) 669 50 61"><?php echo $langArr[1][$lang_marker]; ?> </a>
+				<a class="title1" href="tel:+38 (050) 850 61 23"><?php echo $langArr[2][$lang_marker]; ?></a>
+				<a class="title1" href="mailto:info@pakt3.com.ua"><?php echo $langArr[3][$lang_marker]; ?></a>
+
+			</article>
+			<article class="span6">
+				<h3><span><?php echo $langArr[4][$lang_marker]; ?></span></h3>
+				<p class="margBot1"> <?php echo $langArr[5][$lang_marker]; ?>
+				</p>
+			</article>
+
+		</div>
+		<div class="row">
+			<article class="span12">
+				<p class="title2"><?php echo $langArr[6][$lang_marker]; ?></p>
+				<br>
+				<p class="title2"><?php echo $langArr[7][$lang_marker]; ?></p>
+				<a href="http://foss-art-soft.com/" class="title3">Foss-Art-Soft</a>
+			</article>
+		</div>
+	</div>
+</footer>
+
+		<script>
+			$('#search a').hover(function(){
+				$(this).stop().animate({'opacity':0.5},350, "easeOutSine");
+			}, function(){
+				$(this).stop().animate({'opacity':1},350, "easeOutSine");
+			})
+			$('.follow_icon a').hover(function(){
+				$(this).stop().animate({'opacity':0.5},350, "easeOutSine");
+			}, function(){
+				$(this).stop().animate({'opacity':1},350, "easeOutSine");
+			})
+		</script>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?><?php if( get_marker_of_page()=='ih') :?>	<link href="<?php bloginfo("template_directory"); ?>/css/rtl.css" rel="stylesheet"><?php endif;?>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,172 +1,111 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package pakt3
- */
-change_lang_main();//запуск переключалки(function.php)
-
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Home</title>
-	<meta charset="utf-8">
-	<link rel="icon" href="<?php bloginfo("template_directory"); ?>/img/favicon.ico" type="image/x-icon">
-	<link rel="shortcut icon" href="<?php bloginfo("template_directory"); ?>/img/favicon.ico" type="image/x-icon" />
-	<meta name="description" content="Your description">
-	<meta name="keywords" content="Your keywords">
-	<meta name="author" content="Your name">
-	<meta name = "format-detection" content = "telephone=no" />
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-
-	<!--JS-->
-	<?php wp_head(); ?>
-
-	<script>
-		$(document).ready(function(){
-			jQuery('.camera_wrap').camera();
-			function goToByScroll(id){$('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');}
-		});
-	</script>
-
-	<!--[if lt IE 8]>
-	<div style='text-align:center'>
-		<a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode">
-			<img src="http://www.theie6countdown.com/img/upgrade.jpg"border="0"alt=""/>
-		</a>
-	</div>
-	<![endif]-->
-
-</head>
-<body <?php body_class(); ?> >
-<header>
-	<div class="container">
-		<h1 class="brand">
-			<a href="http://pakt3.foss-art-soft.com/">
-				<?php the_custom_logo(); ?>
-			</a>
-		</h1>
-
-		<div class="container-search">
-			<form id="search" class="search" action="http://pakt3.foss-art-soft.com/"  method="GET" accept-charset="utf-8">
-				<input type="text" onfocus="if(this.value =='' ) this.value=''" onblur="if(this.value=='') this.value=''" value="" name="s">
-				<a href="#" onClick="document.getElementById('search').submit()"><img src="<?php bloginfo("template_directory"); ?>/img/magnify.png" alt=""></a>
-			</form>
-			<form class="lang_switch_form" action="" method="POST">
-				<select class="lang_switch_select" name="lang_switch" id="lang_switch1">
-					<option <?php if(!isset($_COOKIE['lang']) || $_COOKIE['lang']=='ru'){echo 'selected';}?> value="ru">Русский</option>
-					<option <?php if(isset($_COOKIE['lang']) && $_COOKIE['lang']=='en'){echo 'selected';}?> value="en">English</option>
-					<option <?php if(isset($_COOKIE['lang']) && $_COOKIE['lang']=='ua'){echo 'selected';}?> value="ua">Українська</option>
-<!--				<option --><?php //if(isset($_COOKIE['lang']) && $_COOKIE['lang']=='ih'){echo 'selected';}?><!-- value="ih">ייִדיש</option>-->
-
-				</select>
-			</form>
-		</div>
-
-	</div>
-	<div class="container">
-		<div class="navbar navbar_ clearfix">
-			<div class="navbar-inner">
-				<div class="clearfix">
-					<div class="nav-collapse nav-collapse_ collapse">
-							<?php
-							switch(get_marker_of_page()){
-								case 'ru':
-									$menu_id = 'primary';
-									break;
-								case 'en':
-									$menu_id = 'primary_en';
-									break;
-								case 'ua':
-									$menu_id = 'primary_ua';
-									break;
-								case 'ih':
-									$menu_id = 'primary_ih';
-									break;
-								default:
-									$menu_id = 'primary';
-							}
-							wp_nav_menu(array(
-									'theme_location' => $menu_id,
-									'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-									'menu_class' => 'nav sf-menu clearfix',
-									'depth' => 3,
-									'walker' => new magomra_walker_nav_menu()
-							)); ?>
-
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<?php
-		//* Do NOT include the opening php tag
-		// Nav Menu Dropdown Class
-//		include_once('wordpress/wp-content/themes/pakt3/lib/classes/nav-menu-dropdown.php' );
-//		wp_nav_menu( array(
-//			// 'theme_location' => 'mobile',
-//				'menu'           => 'mobile',
-//				'walker'         => new Walker_Nav_Menu_Dropdown(),
-//				'items_wrap'     => '<div class="mobile-menu"><form><select class="select-menu" onchange="if (this.value) window.location.href=this.value">%3$s</select></form></div>'
-//		) );
-		?>
-	</div>
-	<div class="shadow">
-		<div class="container">
-			<div class="row">
-				<article class="span12">
-					<figure><img src="<?php bloginfo("template_directory"); ?>/img/shadow.png" alt="shadow"></figure>
-				</article>
-			</div>
-		</div>
-	</div>
-</header>
-
-
-<!--<!DOCTYPE html>-->
-<!--<html --><?php //language_attributes(); ?><!-->
-<!--<head>-->
-<!--<meta charset="--><?php //bloginfo( 'charset' ); ?><!--">-->
-<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
-<!--<link rel="profile" href="http://gmpg.org/xfn/11">-->
-<!---->
-<?php //wp_head(); ?>
-<!--</head>-->
-<!---->
-<!--<body --><?php //body_class(); ?><!-->
-<!--<div id="page" class="site">-->
-<!--	<a class="skip-link screen-reader-text" href="#content">--><?php //esc_html_e( 'Skip to content', 'pakt3' ); ?><!--</a>-->
-<!---->
-<!--	<header id="masthead" class="site-header" role="banner">-->
-<!--		<div class="site-branding">-->
-<!--			--><?php
-//			if ( is_front_page() && is_home() ) : ?>
-<!--				<h1 class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>-->
-<!--			--><?php //else : ?>
-<!--				<p class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></p>-->
-<!--			--><?php
-//			endif;
-//
-//			$description = get_bloginfo( 'description', 'display' );
-//			if ( $description || is_customize_preview() ) : ?>
-<!--				<p class="site-description">--><?php //echo $description; /* WPCS: xss ok. */ ?><!--</p>-->
-<!--			--><?php
-//			endif; ?>
-<!--		</div><!-- .site-branding -->
-<!---->
-<!--		<nav id="site-navigation" class="main-navigation" role="navigation">-->
-<!--			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">--><?php //esc_html_e( 'Primary Menu', 'pakt3' ); ?><!--</button>-->
-<!--			--><?php //wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-<!--		</nav><!-- #site-navigation -->
-<!--	</header><!-- #masthead -->
-<!---->
-<!--	<div id="content" class="site-content">-->
-
-
+<?php/** * The header for our theme * * This is the template that displays all of the <head> section and everything up until <div id="content"> * * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials * * @package pakt3 */?><?php change_lang_main();//запуск переключалки(function.php)?><!DOCTYPE html>
+<html lang="en">
+<head><title>Home</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="<?php bloginfo("template_directory"); ?>/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php bloginfo("template_directory"); ?>/img/favicon.ico" type="image/x-icon"/>
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">    <!--JS--> <?php wp_head(); ?>
+    <script>        $(document).ready(function () {
+            jQuery('.camera_wrap').camera();
+            function goToByScroll(id) {
+                $('html,body').animate({scrollTop: $("#" + id).offset().top}, 'slow');
+            }
+
+            $(".cameraContent").bind("click", function () {
+                var urlData = $(this).children(".camera-caption").attr("data-url");
+                window.location.href = urlData;
+            });
+        });    </script>
+    <!--[if lt IE 8]>
+    <div style='text-align:center'><a
+        href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"> <img
+        src="http://www.theie6countdown.com/img/upgrade.jpg" border="0" alt=""/> </a></div>    <![endif]--></head>
+<body <?php body_class(); ?> >
+<header>
+    <div class="container"><h1 class="brand"><a
+                href="http://pakt3.foss-art-soft.com/">                <?php the_custom_logo(); ?>            </a></h1>
+        <div class="container-search">
+            <form id="search" class="search" action="http://pakt3.foss-art-soft.com/" method="GET"
+                  accept-charset="utf-8"><input type="text" onfocus="if(this.value =='' ) this.value=''"
+                                                onblur="if(this.value=='') this.value=''" value="" name="s"> <a href="#"
+                                                                                                                onClick="document.getElementById('search').submit()"><img
+                        src="<?php bloginfo("template_directory"); ?>/img/magnify.png" alt=""></a></form>
+            <form class="lang_switch_form" action="" method="POST"><select class="lang_switch_select" name="lang_switch"
+                                                                           id="lang_switch1">
+                    <option <?php if (!isset($_COOKIE['lang']) || $_COOKIE['lang'] == 'ru') {
+                        echo 'selected';
+                    } ?> value="ru">Русский
+                    </option>
+                    <option <?php if (isset($_COOKIE['lang']) && $_COOKIE['lang'] == 'en') {
+                        echo 'selected';
+                    } ?> value="en">English
+                    </option>
+                    <option <?php if (isset($_COOKIE['lang']) && $_COOKIE['lang'] == 'ua') {
+                        echo 'selected';
+                    } ?> value="ua">Українська
+                    </option>
+                    <option <?php if (isset($_COOKIE['lang']) && $_COOKIE['lang'] == 'ih') {
+                        echo 'selected';
+                    } ?> value="ih">ייִדיש
+                    </option>
+                </select></form>
+        </div>
+    </div>
+    <div class="container">
+        <div class="navbar navbar_ clearfix">
+            <div class="navbar-inner">
+                <div class="clearfix">
+                    <div
+                        class="nav-collapse nav-collapse_ collapse">                            <?php switch (get_marker_of_page()) {
+                            case 'ru':
+                                $menu_id = 'primary';
+                                break;
+                            case 'en':
+                                $menu_id = 'primary_en';
+                                break;
+                            case 'ua':
+                                $menu_id = 'primary_ua';
+                                break;
+                            case 'ih':
+                                $menu_id = 'primary_ih';
+                                break;
+                            default:
+                                $menu_id = 'primary';
+                        }
+                        wp_nav_menu(array('theme_location' => $menu_id, 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>', 'menu_class' => 'nav sf-menu clearfix', 'depth' => 3, 'walker' => new magomra_walker_nav_menu())); ?>                    </div>
+                </div>
+            </div>
+        </div> <?php //* Do NOT include the opening php tag		// Nav Menu Dropdown Class//		include_once('wordpress/wp-content/themes/pakt3/lib/classes/nav-menu-dropdown.php' );//		wp_nav_menu( array(//			// 'theme_location' => 'mobile',//				'menu'           => 'mobile',//				'walker'         => new Walker_Nav_Menu_Dropdown(),//				'items_wrap'     => '<div class="mobile-menu"><form><select class="select-menu" onchange="if (this.value) window.location.href=this.value">%3$s</select></form></div>'//		) );		?>
+    </div>
+    <div class="shadow">
+        <div class="container">
+            <div class="row">
+                <article class="span12">
+                    <figure><img src="<?php bloginfo("template_directory"); ?>/img/shadow.png" alt="shadow"></figure>
+                </article>
+            </div>
+        </div>
+    </div>
+</header><!--<!DOCTYPE html>--><!--<html --><?php //language_attributes(); ?><!--><!--<head>--><!--<meta charset="-->
+<?php //bloginfo( 'charset' ); ?><!--">--><!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+<!--<link rel="profile" href="http://gmpg.org/xfn/11">--><!----><?php //wp_head(); ?><!--</head>--><!----><!--<body -->
+<?php //body_class(); ?><!--><!--<div id="page" class="site">-->
+<!--	<a class="skip-link screen-reader-text" href="#content">-->
+<?php //esc_html_e( 'Skip to content', 'pakt3' ); ?><!--</a>--><!---->
+<!--	<header id="masthead" class="site-header" role="banner">--><!--		<div class="site-branding">--><!--			-->
+<?php //			if ( is_front_page() && is_home() ) : ?><!--				<h1 class="site-title"><a href="-->
+<?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>--><!--			-->
+<?php //else : ?><!--				<p class="site-title"><a href="-->
+<?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></p>--><!--			-->
+<?php //			endif;////			$description = get_bloginfo( 'description', 'display' );//			if ( $description || is_customize_preview() ) : ?><!--				<p class="site-description">-->
+<?php //echo $description; /* WPCS: xss ok. */ ?><!--</p>--><!--			-->
+<?php //			endif; ?><!--		</div><!-- .site-branding --><!---->
+<!--		<nav id="site-navigation" class="main-navigation" role="navigation">-->
+<!--			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">-->
+<?php //esc_html_e( 'Primary Menu', 'pakt3' ); ?><!--</button>--><!--			-->
+<?php //wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?><!--		</nav><!-- #site-navigation -->
+<!--	</header><!-- #masthead --><!----><!--	<div id="content" class="site-content">-->
